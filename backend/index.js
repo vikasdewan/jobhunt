42d1d26d2a26a2e20f0b3efd1a87c0a4a75ca72a
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-22 00:35:34 +0530

    `fix: updated frontend serving paths in production mode`

--- backend/index.js
+++ backend/index.js
@@ -34,10 +34,10 @@ app.use("/api/v1/application", applicationRoute);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
   });
 }
 
