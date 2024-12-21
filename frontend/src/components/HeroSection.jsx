ecf636bbf6c3d6e570c552f61b2bf7b95d5fc7b6
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-22 01:03:39 +0530

    `Added a space to the 'text-center' class in HeroSection component`

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
     }
 
     return (
-        <div className='text-center'>
+        <div className='text-center '>
             <div className='flex flex-col gap-5 my-10'>
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
